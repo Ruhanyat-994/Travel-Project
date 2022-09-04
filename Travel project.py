0512023bae23a9085c026206609cac59774c82f0
al-ruhanyat <alruhanyat994@gmail.com>
2022-09-04 17:30:48 +0600

    first commit

---
+++ Travel project.py
@@ -0,0 +1,23 @@
+# Here is step -1
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Sir, please enter your six digit travel number")
+engine.runAndWait()
+
+# Here is step -2
+
+user = int(input("Enter your six digit travel number : "))
+if(user>99999 and user<1000000):
+    import pyttsx3
+    engine = pyttsx3.init()
+    engine.say("Hello Ruhanyat, Have a safe journey!")
+    engine.runAndWait()
+else: 
+# Here is step -3
+    
+    import pyttsx3
+    import time
+    time.sleep(1)
+    engine = pyttsx3.init()
+    engine.say("Sir, please enter a valid travel number")
+    engine.runAndWait()
